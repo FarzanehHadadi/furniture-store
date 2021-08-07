aedf92630433b1c2e1d75577f0f7d3ef35e189b1
farzanehHaddadi <farzhadadi@gmail.com>
2021-08-07 13:01:32 +0430

    styles and packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
+import Navbar from "../src/components/Navbar";
+import Footer from "../src/components/Footer";
 import "../styles/style.scss";
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 const App = () => {
   return (
     <div>
-      <Navbar />
       <Home />
     </div>
   );
