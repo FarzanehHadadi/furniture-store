ff3a9f331954d9c4215697ff989e850ef8d3036f
farzanehHaddadi <farzhadadi@gmail.com>
2021-08-07 23:15:37 +0430

    navbar menus completed

--- src/components/NavbarMenus.jsx
+++ src/components/NavbarMenus.jsx
@@ -4,26 +4,33 @@ import { NavDropdown } from "react-bootstrap";
 
 const NavbarMenus = () => {
   return (
-    <div>
+    <div className="navbar-menus">
       {Categories.map((cat) => {
         return (
           <NavDropdown title={cat.category} id={cat.id}>
-            {cat.products.map((prod) => {
-              return (
-                <ul class="dropdown">
-                  {prod.title}
-                  {prod.subMenu.map((menuItem) => {
-                    return (
-                      <li class="dropdown-item" key={menuItem.id}>
-                        <Link>
-                          <a href="#">{menuItem.desc}</a>
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              );
-            })}
+            <NavDropdown.Item>
+              {cat.products.map((prod) => {
+                return (
+                  <ul className="dropdown">
+                    <p className="product-title">{prod.title}</p>
+                    <hr />
+                    {prod.subMenu.map((menuItem) => {
+                      return (
+                        <li className="dropdown-item" key={menuItem.id}>
+                          {
+                            <Link href="">
+                              {/* <a href="#" color="#604d53"> */}
+                              {menuItem.desc}
+                              {/* </a> */}
+                            </Link>
+                          }
+                        </li>
+                      );
+                    })}
+                  </ul>
+                );
+              })}
+            </NavDropdown.Item>
           </NavDropdown>
         );
       })}
